3da8082b00871ece21b2010f09835a3ea679525d
Gaurank Sharma <95328684+gaurank11@users.noreply.github.com>
2025-01-14 17:56:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Blog Generator API!');
+});
 // Generating Outline Endpoint
 app.post('/generate-outline', async (req, res) => {
     const { title } = req.body;
